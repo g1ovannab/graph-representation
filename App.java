bdc3c4dc43991267da19d1bf61fbd28cd739bdce
Giovanna Bueno <giovanna.bueno.b@gmail.com>
2021-03-04 15:41:58 -0300

    Graphs 1, 2 and 4 implemented with matrix

--- App.java
+++ App.java
@@ -10,7 +10,7 @@ public class App {
         /* Criando um Objeto 'Files' que guarda toda a informação de um arquivo, como
         o próprio arquivo, o FileReader (método que permite leitura) e BufferedReader 
         (método que permite ler linha por linha - read line - até que um \n seja encontrado); */
-        File f1 = new File("files/file1.txt");
+        File f1 = new File("files/graph1.txt");
         /* O caminho dos arquivos já existentes é uma pasta dentro do projeto; */
         FileReader fr1 = new FileReader(f1);
         BufferedReader br1 = new BufferedReader(fr1);
@@ -21,23 +21,21 @@ public class App {
         //Repete-se o processo para os 4 arquivos existentes;
 
         //File 2
-        File f2 = new File("files/file2.txt");
+        File f2 = new File("files/graph2.txt");
         FileReader fr2 = new FileReader(f2);
         BufferedReader br2 = new BufferedReader(fr2);
         Files file2 = new Files(f2, fr2, br2);
         files_list.add(file2);
 
         //File 3
-        File f3 = new File("files/file3.txt");
-        /* O caminho dos arquivos já existentes é uma pasta dentro do projeto; */
+        File f3 = new File("files/graph3.txt");
         FileReader fr3 = new FileReader(f3);
         BufferedReader br3 = new BufferedReader(fr3);
         Files file3 = new Files(f3, fr3, br3);
         files_list.add(file3);
 
         //File 4
-        File f4 = new File("files/file4.txt");
-        /* O caminho dos arquivos já existentes é uma pasta dentro do projeto; */
+        File f4 = new File("files/graph4.txt");
         FileReader fr4 = new FileReader(f4);
         BufferedReader br4 = new BufferedReader(fr4);
         Files file4 = new Files(f4, fr4, br4);
@@ -50,29 +48,150 @@ public class App {
 
         for (Files file : files_list){
             line = file.getBR().readLine();
-            int vertices = Integer.parseInt(line);
-
-            line = file.getBR().readLine();
-            int edges = Integer.parseInt(line);
-
-            line = file.getBR().readLine();
             byte directed = Byte.parseByte(line);
 
             line = file.getBR().readLine();
             byte weighted = Byte.parseByte(line);
 
+            line = file.getBR().readLine();
+            int vertices = Integer.parseInt(line);
+
+            line = file.getBR().readLine();
+            int edges = Integer.parseInt(line);
+
             Graph graph = new Graph(vertices, edges, directed, weighted);
             graphs_list.add(graph);
 
-            //aqui tenho que gravar vértices e arestas
-            
-            // while (file nao acabou){
-            //     continua lendo
-            // }
-            // fecha reader e buffer do arquivo
+
+            String regexVertices = " ";
+
+            line = file.getBR().readLine();
+            String [] sliced = line.split(regexVertices);
+            //preciso desse array?
+
+
+            if (directed == 0 && weighted == 0){
+                int[][] matrix = adjacencyMatrix(graph, file, line);
+
+                System.out.print("MATRIZ GRAFO 1\n");
+                for (int i = 0; i < graph.getVertices(); i++){
+                    for (int j = 0; j < graph.getVertices(); j++){
+                        if (matrix[i][j] == -1){
+                            System.out.print(matrix[i][j] + " ");
+                        }
+                        System.out.print(" " + matrix[i][j] + " ");
+                    }
+                    System.out.print("\n");
+                }
+
+            } else if (directed == 0 && weighted == 1){
+                int[][] matrix = adjacencyMatrix(graph, file, line);
+
+                System.out.print("MATRIZ GRAFO 2\n");
+                for (int i = 0; i < graph.getVertices(); i++){
+                    for (int j = 0; j < graph.getVertices(); j++){
+                        if (matrix[i][j] == -1){
+                            System.out.print(matrix[i][j] + " ");
+                        }
+                        System.out.print(" " + matrix[i][j] + " ");
+                    }
+                    System.out.print("\n");
+                }
+
+            } else if (directed == 1 && weighted == 0){
+                //lista de incidência
+                System.out.println("Lista de Incidência ainda não implementada.");
+
+            } else if (directed == 1 && weighted == 1){
+                int[][] matrix = adjacencyMatrix(graph, file, line);
+
+                System.out.print("MATRIZ GRAFO 3\n");
+                for (int i = 0; i < graph.getVertices(); i++){
+                    for (int j = 0; j < graph.getVertices(); j++){
+                        if (matrix[i][j] == -1){
+                            System.out.print(matrix[i][j] + " ");
+                        }
+                        System.out.print(" " + matrix[i][j] + " ");
+                    }
+                    System.out.print("\n");
+                }             
+            }
+
         }
 
 
+        
         scan.close();
     }
+
+    public static int[][] adjacencyMatrix(Graph graph, Files file, String lineRead) throws IOException {
+        //Pensar nos 3 casos que vamos utilizar: 
+            // N DIR E N PON
+            // N DIR E PON
+            // DIR E PON
+        
+        boolean directed = graph.isDirected();
+        boolean weighted = graph.isWeighted();
+
+        int vertices = graph.getVertices();
+
+
+        int[][] adjMatrix = new int[vertices][vertices];
+
+        if (weighted == true){ /*Se for ponderado, vamos preencher a matriz com -1, assim, se houver
+            algum ponderado com o valor 0, saberemos distinguir.*/
+            for (int line = 0; line < vertices; line++){
+                for (int column = 0; column < vertices; column++){
+                    adjMatrix[line][column] = -1;
+                }
+            }
+        } else { /*Se não for ponderado, vamos preencher a matriz com 0 normalmente.*/
+            for (int line = 0; line < vertices; line++){
+                for (int column = 0; column < vertices; column++){
+                    adjMatrix[line][column] = 0;
+                }
+            }
+        }
+        
+
+        lineRead = file.getBR().readLine();
+        // System.out.println(lineRead);
+        
+        while(lineRead != null){
+            String regexEdges = " ";
+
+            String[] slicedEdges = lineRead.split(regexEdges);
+
+            int line = Integer.parseInt(slicedEdges[0]); 
+            int column = Integer.parseInt(slicedEdges[1]);
+
+            if (slicedEdges.length == 2){ /*Se o conjunto de arestas tiver somente 2 valores, significa que não é
+                ponderado. Isso quer dizer que preencheremos o valor da matriz com 1.*/
+
+                adjMatrix[line - 1][column - 1] = 1;
+
+                if (directed == false){
+                    adjMatrix[column - 1][line - 1] = 1;
+                }
+
+            } else if (slicedEdges.length == 3){ /*Se o conjunto de arestas tiver 3 valores, significa que é 
+                ponderado. Iso quer ddizer que preencheremos o valor da matriz com o valor do ponderado.*/
+                
+                int value = Integer.parseInt(slicedEdges[2]);
+                adjMatrix[line - 1][column - 1] = value;
+
+                if (directed == false){
+                    adjMatrix[column - 1][line - 1] = value;
+                }
+            }
+
+            lineRead = file.getBR().readLine();
+        }
+
+        return adjMatrix;
+    }
+
+
+    
+
 }
